92c657d108c190a2979bb7eabb7f1a47c1b5aa99
AkramRamoul <ahmed.ramoul@univ-constantine2.dz>
2024-08-24 14:29:59 +0100

    first commit

---
+++ actions/form.ts
@@ -0,0 +1,34 @@
+"use server";
+class UserNotFoundError extends Error {}
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+export async function GetFormStats() {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundError();
+  }
+  const stats = prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+  const visits = (await stats)._sum.visits || 0;
+  const submissions = (await stats)._sum.submissions || 0;
+
+  let submissionRate = 0;
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+  const bounceRate = 100 - submissionRate;
+
+  return {
+    visits,
+    submissionRate,
+    submissions,
+    bounceRate,
+  };
+}
